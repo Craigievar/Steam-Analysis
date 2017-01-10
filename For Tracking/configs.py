71f709741183931aed06aff76619f9b2ee571f5b
Craig Sexauer <craigsexauer@gmail.com>
2017-01-10 11:30:18 -0800

    Update configs.py

--- For Tracking/configs.py
+++ For Tracking/configs.py
@@ -1,4 +1,4 @@
 #links to document folder - private info
 #will upload db file to drive (sqlite3 .db file)
-keyLoc = "../../../key.txt"
-dbName = "../../../SteamAnalysis.db"
+keyLoc = "key.txt"
+dbName = "SteamAnalysis.db"
