c314c4a933cad47a3a0fac3627fd29592d40d4a4
Craig Sexauer <craigsexauer@gmail.com>
2017-01-10 11:09:30 -0800

    updated caller for remote host
    - Split out database for user-games into a function
    - Doesn't record games with 0 playtime

--- For Tracking/caller.py
+++ For Tracking/caller.py
@@ -1,6 +1,8 @@
-from urllib import request
+import urllib
 import json, sqlite3, time, random
 import sys
+from datetime import datetime
+import traceback
 
 class caller:
     def __init__(self, key, dbName):
@@ -10,8 +12,8 @@ class caller:
     def getResp(self, userId, domain, method, version):
 
         if(method == "GetPlayerSummaries"): idName = "&steamids="
-        elif(method=="GetFriendList"): idName = "&relationship=friend&steamid=" 
-        else: idName = "&steamid=" 
+        elif(method=="GetFriendList"): idName = "&relationship=friend&steamid="
+        else: idName = "&steamid="
 
         url = "http://api.steampowered.com/" +\
             domain +\
@@ -26,33 +28,39 @@ class caller:
             "&format=json"
         if(method == "GetOwnedGames"): url = url + "&include_played_free_games=1"
 
-        resp = request.urlopen(url)
+        resp = urllib.urlopen(url)
         parsedResp = json.loads(resp.read().decode('ascii', 'ignore'))
         return parsedResp
 
+
+    def insertGame(self, userId, game):
+        with self.conn:
+            cur = self.conn.cursor()
+            if int(game['playtime_forever']) > 0:
+			    cur.execute("INSERT INTO user_games VALUES("+\
+			        str(userId)+","+\
+			        str(game['appid'])+","+\
+			        "'" + str(datetime.fromtimestamp(time.time())) + "',"+\
+			        str(game['playtime_forever'])+")")
+
+
     def getGames(self):
-        ts = time.time()
         cursor = self.conn.cursor()
         idList = cursor.execute('SELECT distinct user_id from users').fetchall()
         ids = [entry[0] for entry in idList]
-        ct = 0 
+        ct = 0
         # just pulled all the ids from our id list. Steam chokes @100k calls a day so we'll do 30k x 3
         for userId in ids:
             ct = ct + 1
             try:
-                gameListParsed = self.getResp(userId, "IPlayerService", "GetOwnedGames", "v0001")   
+                gameListParsed = self.getResp(userId, "IPlayerService", "GetOwnedGames", "v0001")
                 if('games' in gameListParsed['response']):
                     gameList = gameListParsed['response']['games']
-                    with self.conn:
-                        cur = self.conn.cursor()
-                        for g in gameList:
-                            cur.execute("INSERT INTO user_games VALUES("+\
-                                str(userId)+","+\
-                                str(g['appid'])+","+\
-                                str(ts)+","+\
-                                str(g['playtime_forever'])+")")
-            except: 
-                print("Error updating " + str(userId))
+                    for game in gameList:
+                        self.insertGame(userId, game)
+            except:
+                traceback.print_exc()
+                #print("Error updating " + str(userId))
             if ct%5000 == 0:
                     print(ct)
 
@@ -67,7 +75,7 @@ class caller:
                 testct = testct + 1
                 #user info
                 thisId = p['steamid']
-                
+
                 if 'loccountrycode' in p:
                     country = p['loccountrycode']
                 else:
